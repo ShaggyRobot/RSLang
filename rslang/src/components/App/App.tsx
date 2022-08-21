2661c0d8ff92ac9028c60a6b7d880d1529d205db
AnatoliyIliev <vekok12@gmail.com>
2022-08-21 20:45:25 +0300

    2.1. Auth feat: add pages SignIn and SignUp

--- rslang/src/components/App/App.tsx
+++ rslang/src/components/App/App.tsx
@@ -12,24 +12,30 @@ import { TextbookPage } from '../../pages/TextbookPage';
 import { NotFoundPage } from '../../pages/NotFoundPage';
 import { AudioCallPage } from '../../pages/AudioCallPage';
 import { StatisticsPage } from '../../pages/StatisticsPage';
+import { SignIn } from '../../pages/SignIn';
+import { SignUp } from '../../pages/SignUp';
 import { Layout } from '../Layout/Layout';
 
 function App(): JSX.Element {
   const authorized = useSelector((state: RootState) => state.authorized);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path='about' element={<AboutPage />} />
-          <Route path='textbook' element={<TextbookPage />} />
-          <Route path='statistics' element={<StatisticsPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="textbook" element={<TextbookPage />} />
+          <Route path="statistics" element={<StatisticsPage />} />
 
-          <Route path='sprint' element={<SprintPage />} />
-          <Route path='audiocall' element={<AudioCallPage />} />
+          <Route path="sprint" element={<SprintPage />} />
+          <Route path="audiocall" element={<AudioCallPage />} />
 
-          <Route path='*' element={<NotFoundPage />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="singup" element={<SignUp />} />
+          {/* <Route path="audiocall" element={<AudioCallPage />} /> */}
+
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </div>
---
+++ rslang/src/components/Layout/AuthNav.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router-dom';
+
+import styles from './LayoutStyles.module.scss';
+
+const AuthNav = (): JSX.Element => (
+  <div style={{ display: 'flex', gap: '1rem', justifyContent: 'center' }}>
+    <NavLink end to="/signin" className={styles.link}>
+      Sign in
+    </NavLink>
+    <NavLink end to="/singup" className={styles.link}>
+      Sign up
+    </NavLink>
+  </div>
+);
+
+export default AuthNav;
